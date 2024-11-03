a606d73d227395a06f67b65223594b2b251de6f6
MohaMed ALshafe <elm06542@gmail.com>
2024-11-03 23:27:17 +0200

    react task 2

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -2,7 +2,8 @@ function WelcomeMessage() {
   return (
     <div>
       <h1>Hello everyone, I am learning React at ALX!</h1>
-      <p>Hello everyone, I am learning React at ALX!.</p>
+      <p>This is a simple JSX component.</p>
+      <p>I am learning about JSX!.</p>
     </div>
   );
 }
