13ebf99a0a5fd7913dbecb1260d114370014a01f
MohaMed ALshafe <elm06542@gmail.com>
2024-11-03 23:34:16 +0200

    react task 3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from "./WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/mainContent";
+import Footer from "./components/Footer";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -9,6 +12,9 @@ function App() {
   return (
     <>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
