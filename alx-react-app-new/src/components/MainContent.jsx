d1d05ee001c285acbc544628e805627c169b929f
MohaMed ALshafe <elm06542@gmail.com>
2024-11-17 22:09:43 +0200

    react task

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MainContent = () => {
   return (
-    <MainContent
+    <main
       style={{
         backgroundColor: "#333",
         color: "white",
@@ -10,7 +10,7 @@ const MainContent = () => {
       }}
     >
       I love to visit New York, Paris, and Tokyo.
-    </MainContent>
+    </main>
   );
 };
 
