5f53fa36e15454aca3c0f37cfab256e12346a5a0
MohaMed ALshafe <elm06542@gmail.com>
2024-11-17 23:39:08 +0200

    last react

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        padding: "20px",
+        textAlign: "center",
+        backgroundColor: "#282c34",
+        color: "white",
+      }}
+    >
+      <p>© 2023 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
