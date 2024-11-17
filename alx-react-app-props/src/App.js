cf5dd365fd9b5ed4a48bdaa561d0783d662d10c9
MohaMed ALshafe <elm06542@gmail.com>
2024-11-17 23:14:40 +0200

    task context 4

--- alx-react-app-props/src/App.js
+++ alx-react-app-props/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import UserContext from "./UserContext";
+import UserContext from "./components/UserContext";
 import ProfilePage from "./ProfilePage";
 
 const App = () => {
