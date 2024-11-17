124a12e305be67e9b13eb07b63881f0d65aedbf3
MohaMed ALshafe <elm06542@gmail.com>
2024-11-17 22:01:20 +0200

    react task 2

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <footer
+      style={{ backgroundColor: "navy", color: "white", textAlign: "center" }}
+    >
+      <p>Footer</p>
+    </footer>
+  );
+};
+
+export default footer;
