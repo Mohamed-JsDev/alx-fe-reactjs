222a446e448178ed7d70bb3e3f65be521d22058d
MohaMed ALshafe <elm06542@gmail.com>
2024-11-24 13:57:45 +0200

    recipe task

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,13 @@
-// DeleteRecipeButton.js
 import { useRecipeStore } from "./recipeStore";
+import { useNavigate } from "react-router-dom";
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const { deleteRecipe } = useRecipeStore();
+  const navigate = useNavigate(); // إضافة useNavigate
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
+    navigate("/"); // إعادة التوجيه بعد الحذف
   };
 
   return <button onClick={handleDelete}>Delete Recipe</button>;
