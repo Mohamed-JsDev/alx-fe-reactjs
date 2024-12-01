6a374978e30d8c08b40235b88ee62841f12c2321
MohaMed ALshafe <elm06542@gmail.com>
2024-12-01 11:07:40 +0200

    advanced react task

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,23 +1,9 @@
 // src/App.jsx
 import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
 import Profile from "./components/profile";
 
 const App = () => {
-  return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-  );
+  return <Profile />;
 };
 
 export default App;
