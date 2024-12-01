e00d3db6b9522aa4bbffe20924988dc83c9a5084
MohaMed ALshafe <elm06542@gmail.com>
2024-12-01 12:12:55 +0200

    just do

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,10 @@
 // src/components/ProtectedRoute.jsx
 import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth"; // تأكد من المسار الصحيح
 
-const ProtectedRoute = ({ element, isAuthenticated }) => {
+const ProtectedRoute = ({ element }) => {
+  const { isAuthenticated } = useAuth();
+
   return isAuthenticated ? element : <Navigate to="/" />;
 };
 
