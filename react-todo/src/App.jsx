f7c19d270635d040f382d4d4deeea4d65b220688
MohaMed ALshafe <elm06542@gmail.com>
2024-12-01 12:34:39 +0200

    last

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,8 +1,7 @@
 import TodoList from "./TodoList";
-import { render, screen, fireEvent } from '@testing-library/react';
-import TodoList from '../TodoList'; // Adjust the path if necessary
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../TodoList"; // Adjust the path if necessary
 const App = () => {
-  use sr
   return (
     <div>
       <TodoList />
