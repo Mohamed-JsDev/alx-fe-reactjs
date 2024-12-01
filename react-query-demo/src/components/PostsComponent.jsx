9a3208d9e59bf35fc4ab321087a992dde443fa81
MohaMed ALshafe <elm06542@gmail.com>
2024-12-01 09:51:54 +0200

    advanced react task

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,19 +1,19 @@
 import { useQuery } from "react-query";
-const fetchData = async () => {
+const fetchPosts = async () => {
   const res = await fetch(" https://jsonplaceholder.typicode.com/posts");
   return res.json();
 };
 const PostsComponent = () => {
-  const { data, error, isLoading } = useQuery("fetchData", fetchData);
+  const { data, isError, isLoading } = useQuery("fetchPosts", fetchPosts);
   if (isLoading) return <div>loading...</div>;
-  if (error) return <div>error loading data</div>;
+  if (isError) return <div>error loading data</div>;
 
   return (
     <div>
       {data.map((item) => (
         <div key={item.id}>{item.name}</div>
       ))}
-      <button onClick={fetchData}>refresh</button>
+      <button onClick={fetchPosts}>refresh</button>
     </div>
   );
 };
