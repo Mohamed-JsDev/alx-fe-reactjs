c662ed1f4a2c79aa587145b8acdefb39dfff4a3d
MohaMed ALshafe <elm06542@gmail.com>
2024-12-01 12:15:13 +0200

    just do

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,13 +1,33 @@
 // src/App.jsx
-// import { Routes, Route,  Link, BrowserRouter } from "react-router-dom";
-import Profile from "./components/profile";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./components/Profile";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
+import useAuth from "./hooks/useAuth";
 
 const App = () => {
-  return;
-  <>
-    <Profile />
-    <Route path="/blog/:id" element="BlogPost" />
-  </>;
+  const { isAuthenticated } = useAuth(); // استخدام useAuth للحصول على حالة المصادقة
+
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+        <button onClick={() => (isAuthenticated ? logout() : login())}>
+          {isAuthenticated ? "Logout" : "Login"}
+        </button>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/profile/*"
+          element={<ProtectedRoute element={<Profile />} />}
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
