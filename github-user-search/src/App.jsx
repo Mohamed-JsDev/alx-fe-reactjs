67bbde14680b021f0a2e6dff20bb6a9798875513
MohaMed ALshafe <elm06542@gmail.com>
2024-12-15 14:37:50 +0200

    done

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+      <h1>Mohamed</h1>
+    </>
+  );
+}
+
+export default App;
