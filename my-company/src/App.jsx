18a9fe7f77879a507f76382b5dd3792974e5e8bd
Mohamed Alshafie <122812170+Mohamed-JsDev@users.noreply.github.com>
2025-03-12 01:17:18 +0200

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
-import Services from "./Services";
+// import Services from "./Services";/
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Footer from "./Footer";
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
+{/*           <Route path="/services" element={<Services />} /> */}
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
